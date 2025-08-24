e1543652af42cf793833d890dbc91f5c7b42231c
Leandro Rodriguez <leandro-1.0@hotmail.com>
2025-08-24 16:19:29 -0300

    Solución al problema de propagación del click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,14 @@
-
 const contenedor = document.getElementById("contenedor");
+const boton = document.getElementById("btnSaludar");
+
 
 contenedor.addEventListener("click", function() {
   alert("Hola! Soy el div");
 });
+
+
+boton.addEventListener("click", function(event) {
+  event.stopPropagation(); 
+  alert("Hola!");
+});
+
